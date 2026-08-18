349c5de2522df0f4c2e27ce93729445a87788b0d
Dishna Daham Wijemanna <dishnadahamwijemanna@gmail.com>
2026-08-18 14:04:06 +0530

    week7 q11

--- DAY 6/n/Assignment6/Example11.java
+++ DAY 6/n/Assignment6/Example11.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+class Example11{
+		public static void celconverter(double c){
+				double f = c*(9/5)+32;
+				double k = c+ 273.15;
+				
+				System.out.println("fahrenheit - "+f);
+				System.out.print("Celsius - "+k);
+				
+		}
+		public static void main(String args[]){
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Enter celsios value : ");
+				double c = sc.nextDouble();
+				
+				celconverter(c);
+		}
+}
